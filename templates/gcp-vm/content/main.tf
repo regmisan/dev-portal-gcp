01ecaf365240cd85872d3428af4e1fd43970dafc
regmisan <40816347+regmisan@users.noreply.github.com>
2025-03-31 16:34:32 +0545

    Create main.tf

---
+++ templates/gcp-vm/content/main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "default" {
+  name         = var.instance_name
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
